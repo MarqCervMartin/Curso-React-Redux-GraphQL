405d445b5682ee308ba85b073ac68c362ec6bc40
MarqCervMartin <martin_mcervantes@hotmail.com>
2021-12-24 12:53:54 -0600

    redux config and characters

---
+++ src/redux/charDuck.js
@@ -0,0 +1,53 @@
+import axios from 'axios';
+
+//constants
+let initialData = {
+    fetching: false,
+    array: [],
+    current: {}
+}
+
+let URL = "https://rickandmortyapi.com/api/character";
+
+let GET_CHARACTERS = "GET_CHARACTERS";
+let GET_CHARACTERS_SUCCESS = "GET_CHARACTERS_SUCCESS";
+let GET_CHARACTERS_ERROR = "GET_CHARACTERS_ERROR";
+
+
+//reducer
+export default function reducer(state=initialData, action) {
+    switch(action.type) {
+        case GET_CHARACTERS:
+            return {...state, fetching: true}
+        case GET_CHARACTERS_ERROR:
+            return {...state, error: action.payload, fetching: false}
+        case GET_CHARACTERS_SUCCESS:
+            return {...state, array: action.payload, fetching: false}
+        default:
+            return state;
+    };
+};
+
+//actions (thunk)
+export function getCharactersAction(){
+    return (dispatch, getState) => {
+        //set fetching in true
+        dispatch({
+            type: GET_CHARACTERS,
+        });
+        return axios.get(URL)
+            .then(res => {
+                dispatch({
+                    type: GET_CHARACTERS_SUCCESS,
+                    payload: res.data.results,
+                });
+            })
+            .catch(error => {
+                console.log(error);
+                dispatch({
+                    type: GET_CHARACTERS_ERROR,
+                    payload: error.response.massage
+                })
+            })
+    };
+};
