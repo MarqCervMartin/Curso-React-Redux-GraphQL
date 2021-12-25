1543ded01735879c1b7af6cb687725374322e176
MarqCervMartin <martin_mcervantes@hotmail.com>
2021-12-24 20:10:31 -0600

    add connect and remove characters

--- src/redux/charDuck.js
+++ src/redux/charDuck.js
@@ -13,10 +13,13 @@ let GET_CHARACTERS = "GET_CHARACTERS";
 let GET_CHARACTERS_SUCCESS = "GET_CHARACTERS_SUCCESS";
 let GET_CHARACTERS_ERROR = "GET_CHARACTERS_ERROR";
 
+let REMOVE_CHARACTER = "REMOVE_CHARACTER";
 
 //reducer
 export default function reducer(state=initialData, action) {
     switch(action.type) {
+        case REMOVE_CHARACTER:
+            return {...state, array: action.payload}
         case GET_CHARACTERS:
             return {...state, fetching: true}
         case GET_CHARACTERS_ERROR:
@@ -29,6 +32,16 @@ export default function reducer(state=initialData, action) {
 };
 
 //actions (thunk)
+
+export let removeCharacterAction = () => (dispatch, getState) => {
+    let {array} = getState().characters;
+    array.shift();
+    dispatch({
+        type: REMOVE_CHARACTER,
+        payload: [...array]
+    });
+}
+
 export function getCharactersAction(){
     return (dispatch, getState) => {
         //set fetching in true
